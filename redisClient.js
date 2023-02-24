029661d8567a974ba9e4fa5f3032accd6ba139d9
Ross Brown <albatrocity@gmail.com>
2023-02-23 22:14:45 -0600

    fixes client accessor

--- redisClient.js
+++ redisClient.js
@@ -1,9 +1,6 @@
 const { createClient: redisCreateClient } = require("redis");
 
 async function createClient() {
-  if (client) {
-    return client;
-  }
   const client = redisCreateClient({
     url: process.env.REDIS_URL || "redis://127.0.0.1:6379",
   });
