56c29ee633e0177880e9577352646cb2ca92ea2d
Ross Brown <ross_brown@external.mckinsey.com>
2023-04-13 11:37:53 -0500

    connects to redis clients

--- server.ts
+++ server.ts
@@ -57,6 +57,8 @@ const pubClient = createClient({
   url: process.env.REDIS_URL || "redis://127.0.0.1:6379",
 });
 const subClient = pubClient.duplicate();
+pubClient.connect();
+subClient.connect();
 
 io.adapter(createAdapter(pubClient, subClient));
 
