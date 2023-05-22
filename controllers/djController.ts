f2310117a0c6f7963fea23227f41c281fb990cc7
Ross Brown <ross_brown@external.mckinsey.com>
2023-05-22 15:17:25 -0500

    supports lifecycle events, deputizeOnJoin setting

--- controllers/djController.ts
+++ controllers/djController.ts
@@ -5,7 +5,9 @@ import {
   queueSong,
   searchSpotifyTrack,
   setDj,
+  handleUserJoined,
 } from "../handlers/djHandlers";
+import { events } from "../lib/eventEmitter";
 import { SpotifyEntity } from "../types/SpotifyEntity";
 import { User } from "../types/User";
 
@@ -15,7 +17,7 @@ export default function djController(socket: Socket, io: Server) {
   );
 
   socket.on("dj deputize user", (userId: User["userId"]) =>
-    djDeputizeUser({ socket, io }, userId)
+    djDeputizeUser({ io }, userId)
   );
 
   socket.on("queue song", (uri: SpotifyEntity["uri"]) =>
@@ -25,4 +27,11 @@ export default function djController(socket: Socket, io: Server) {
   socket.on("search spotify track", (query: { query: string; options: any }) =>
     searchSpotifyTrack({ socket, io }, query)
   );
+}
+
+export function lifecycleEvents(io: Server) {
+  events.on("USER_JOINED", (data: { user: User; users: User[] }) => {
+    console.log("handle user joined event");
+    handleUserJoined({ io }, { user: data.user, users: data.users });
+  });
 }
