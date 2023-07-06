007b043c3decab50922d27cc008c4dc0bf5f0e97
Ross Brown <ross@crema.us>
2023-07-06 15:32:05 -0500

    removes session middleware

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,7 @@
 import { createAdapter } from "@socket.io/redis-adapter";
 import cookieParser from "cookie-parser";
-import session from "express-session";
 import cors from "cors";
-import express, { Request, Response } from "express";
-import RedisStore from "connect-redis";
+import express from "express";
 import { Server } from "socket.io";
 
 import { FORTY_FIVE_MINS } from "./lib/constants";
@@ -31,21 +29,8 @@ const PORT = Number(process.env.PORT ?? 3000);
 
 const streamURL = process.env.SERVER_URL;
 
-const redisStore = new RedisStore({
-  client: pubClient,
-  prefix: "session:",
-});
-
-const sessionMiddleware = session({
-  store: redisStore,
-  secret: process.env.SESSION_SECRET || "secret",
-  saveUninitialized: true,
-  resave: true,
-});
-
 const httpServer = express()
   .use(express.static(__dirname + "/public"))
-  .use(sessionMiddleware)
   .use(cors())
   .use(express.json())
   .use(cookieParser())
@@ -69,41 +54,12 @@ const io = new Server(httpServer, {
   pingTimeout: 60000,
   pingInterval: 25000,
   allowEIO3: false,
-  allowRequest: (req, callback) => {
-    // with HTTP long-polling, we have access to the HTTP response here, but this is not
-    // the case with WebSocket, so we provide a dummy response object
-    const fakeRes = {
-      getHeader() {
-        return [];
-      },
-      // setHeader(key: string, values: string[]) {
-      //     req.cookieHolder = values[0];
-      // },
-      writeHead() {},
-    };
-    sessionMiddleware(req as Request, fakeRes as unknown as Response, () => {
-      if (req.session) {
-        // trigger the setHeader() above
-        fakeRes.writeHead();
-        // manually save the session (normally triggered by res.end())
-        req.session.save();
-      }
-      callback(null, true);
-    });
-  },
 });
 
 pubClient.connect();
 subClient.connect();
 
 io.adapter(createAdapter(pubClient, subClient));
-io.engine.use(sessionMiddleware);
-// io.engine.on("initial_headers", (headers: { [key: string]: string }, req: IncomingMessage) => {
-//   if (req.cookieHolder) {
-//       headers["set-cookie"] = req.cookieHolder;
-//       delete req.cookieHolder;
-//   }
-// })
 
 let offline = true;
 let oAuthInterval: NodeJS.Timer | null;
@@ -111,16 +67,6 @@ let oAuthInterval: NodeJS.Timer | null;
 io.on("connection", (socket) => {
   // @ts-ignore
   const req = socket.request;
-  // req.session.socketId = socket.id;
-  socket.use((__, next) => {
-    req.session.reload((err) => {
-      if (err) {
-        socket.disconnect();
-      } else {
-        next();
-      }
-    });
-  });
   authController(socket, io);
   messageController(socket, io);
   activityController(socket, io);
