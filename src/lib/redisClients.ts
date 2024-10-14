68d399ffea203b345b656aa1cd01f395d8cb66c6
Ross Brown <albatrocity@gmail.com>
2024-10-14 10:02:56 -0500

    adds error handlers to pub/sub clients

--- src/lib/redisClients.ts
+++ src/lib/redisClients.ts
@@ -4,3 +4,13 @@ export const pubClient = createClient({
   url: process.env.REDIS_URL ?? "redis://127.0.0.1:6379",
 });
 export const subClient = pubClient.duplicate();
+
+pubClient.on("error", (error) => {
+  console.log("pubClient error");
+  console.error(error);
+});
+
+subClient.on("error", (error) => {
+  console.log("subClient error");
+  console.error(error);
+});
