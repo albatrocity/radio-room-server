d4e4b801512b59a7f23e4e26247bd958d9c8b619
Ross Brown <albatrocity@gmail.com>
2025-02-18 08:56:23 -0600

    fixes redis env var

--- src/jobs/redis.ts
+++ src/jobs/redis.ts
@@ -1,7 +1,7 @@
 import { createClient } from "redis";
 
 export const client = createClient({
-  url: process.env.REDIS_TLS_URL ?? "redis://127.0.0.1:6379",
+  url: process.env.REDIS_URL ?? "redis://127.0.0.1:6379",
   socket:
     process.env.NODE_ENV === "production"
       ? {
